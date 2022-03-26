4dd9158980765b910adee41d4664ae04f1033d7c
Napolesllll <canojhon148@gmal.com>
2022-03-25 22:47:34 -0500

    bug del back resuelto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -30,8 +30,8 @@ const getApiInfo = async () => {
 
 //// trayendo bases de datos
 const getDbInfo = async () => {
-    return await Dog.findAll({    //me traigo la info de la base de datos del modelo Dog que incluye el mod Temperament
-        include:{             //porque si no lo incluyo al crear un dog nunca me va a traer el dog con el temperamento
+    return await Dog.findAll({          //me traigo la info de la base de datos del modelo Dog que incluye el mod Temperament
+        include:{                      //porque si no lo incluyo al crear un dog nunca me va a traer el dog con el temperamento
             model: Temperament,
             attributes: ['name'],
             through: {
@@ -44,21 +44,21 @@ const getDbInfo = async () => {
 const getAllDogs = async () => {
     const apiInfo = await getApiInfo();
     const dbInfo = await getDbInfo();
-    const allInfo = apiInfo.concat(dbInfo);                    ////concatenando los datos de la api y DB
+    const allInfo = apiInfo.concat(dbInfo);      ////concatenando los datos de la api y DB
 
     return allInfo;
 }
 
 router.get('/dogs', async (req, res) => {
     const name = req.query.name
-    let totalDogs = await getAllDogs();               //me traigo todos, Db y api
-    if(name){                                           // si hay un nombre por query
+    let totalDogs = await getAllDogs();      //me traigo todos, Db y api
+    if(name){                               // si hay un nombre por query
         let dogName = await totalDogs.filter( el => el.name.toLowerCase().includes(name.toLowerCase()));
-        dogName.length ?                                  //si hay algún nombre
+        dogName.length ?                          //si hay algún nombre
         res.status(200).send(dogName) :
         res.status(404).send('Doggy Not Found');
     }else {
-        res.status(200).send(totalDogs);      //si no hay name por query manda un status 200 con todos los dogs
+        res.status(200).send(totalDogs);   //si no hay name por query manda un status 200 con todos los dogs
     }
 })
 
@@ -66,8 +66,8 @@ router.get('/dogs/:id', async (req, res) => {
     const id = req.params.id;
     const allDogs = await getAllDogs();
     if(id){
-        let dogId = await allDogs.filter(el => el.id == id); //dentro de todos los dogs filtra el id que te estoy pasando 
-        dogId.length ? res.json(dogId) : res.status(404).send('Doggy Not Found'); //si no encuentra nada entra en la res.status
+        let dogId = await allDogs.filter(el => el.id == id);   //dentro de todos los dogs filtra el id que te estoy pasando 
+        dogId.length ? res.json(dogId) : res.status(404).send('Doggy Not Found');  //si no encuentra nada entra en la res.status
     }
 })
 
@@ -77,9 +77,9 @@ router.get('/temperament', async (req, res) => {
 
     const temperament = temperamentApi.data.map(el => el.temperament).join(", ").split(", ").join(", ").split(", ")
 
-    await temperament.forEach( el => { //para cada uno de ellos entrá al modelo Temperament y hacé un findOrCreate
+    await temperament.forEach( el => {   //para cada uno de ellos entrá al modelo Temperament y hacé un findOrCreate
         Temperament.findOrCreate ({   // es un método de sequelize usado para chequear si un elemento ya existe en la Db, y si no existe, lo va a crear.
-            where: { name: el }                 //creáme estos temperamentos donde el nombre sea este elemento que estoy mapeando
+            where: { name: el }          //creáme estos temperamentos donde el nombre sea este elemento que estoy mapeando
         })
     });
     const dogTemperament = await Temperament.findAll();
@@ -95,9 +95,9 @@ router.post('/dog', async (req, res) => {
         image,
         createdInDb,
         temperament,
-    }= req.body                         //me traigo del body todo lo que necesito
+    }= req.body                              //me traigo del body todo lo que necesito
 
-    let dogCreated = await Dog.create({//creo el dog con el modelo Dog y le paso lo mismo excepto el temperament porque lo tengo que encontrar en un modelo que ya tengo
+    let dogCreated = await Dog.create({  //creo el dog con el modelo Dog y le paso lo mismo excepto el temperament porque lo tengo que encontrar en un modelo que ya tengo
         name,
         height,
         weight,
@@ -106,13 +106,13 @@ router.post('/dog', async (req, res) => {
         createdInDb,
         })
 
-    let temperamentDb = await Temperament.findAll({  //dentro de mi modelo encontrá todos los temperament que coincidan con lo que le paso por body
+    let temperamentDb = await Temperament.findAll({ //dentro de mi modelo encontrá todos los temperament que coincidan con lo que le paso por body
         where: {
-            name: temperament,  //name es igual al temperament que le llega por body
+            name: temperament,    //name es igual al temperament que le llega por body
         }
     });
 
-    dogCreated.addTemperament(temperamentDb)  //al dog creado agregále el temperament encontrado en la Bd que le llegó por body
+    dogCreated.addTemperament(temperamentDb) //al dog creado agregále el temperament encontrado en la Bd que le llegó por body
     res.send('Successfull Created Doggy')
 })
 
